bc9f8fbc8217a575f213563493e6f7d1cf9046a3
andreferrazz <oandreferraz@gmail.com>
2024-01-04 00:29:59 -0300

    Add prettier config.

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    "trailingComma": "es5",
+    "tabWidth": 4,
+    "semi": false,
+    "doubleQuote": true,
+    // max columns
+    "printWidth": 100,
+}
+
+export default config;
